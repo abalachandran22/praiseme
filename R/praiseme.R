9e795c185f323c0425ab72b7d1301a2bbdab6142
abalachadran22 <a.balachandran@uqconnect.edu.au>
2019-12-05 16:15:23 +1100

    deleted manual for hello.R and wrote manual for praiseme function

--- R/praiseme.R
+++ R/praiseme.R
@@ -1,3 +1,15 @@
+#' Praise the user
+#'
+#' \code{praiseme} returns praise incorporating a user-specified string.
+#'
+#' @param your_name A string specifying the name of the praisee.
+#'
+#' @return A string with the praise.
+#' @export
+#'
+#' @examples
+#' praiseme("Luke")
+
 praiseme <- function(your_name){
 
     paste(your_name, ",so far so good!")
